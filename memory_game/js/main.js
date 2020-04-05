15b34f23cade9195e15865b9391bede67f96fcbb
Samuel Gemberling <sgemberling@gmail.com>
2020-04-04 19:45:43 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,22 +4,26 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[1];
-let cardThree = cards[2];
-let cardFour = cards[3];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if((cardsInPlay[0] === cardsInPlay[1]) && cardsInPlay.length === 2)
+function checkForMatch(){
+if(cardsInPlay[0] === cardsInPlay[1] && cardsInPlay.length === 2)
 {
-	alert("You've found a match!");
+	console.log("You've found a match!");
 }
 else
 {
-	alert("Sorry try again.")
+	console.log("Sorry try again.");
+}	
 }
+
+function flipCard(cardId){
+
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch();
+}
+
+
+flipCard(0);
+flipCard(2);
